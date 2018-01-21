555269cdfa223dda0f8d4435dda57268a143421f
somkiat <somkiat.p@gmail.com>
2018-01-21 20:59:45 +0700

    Demo

---
+++ src/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+  "fmt"
+  "github.com/aws/aws-lambda-go/lambda"
+)
+
+type Request struct {
+  ID float64 `json:"id"`
+  Value string `json:"value"`
+}
+
+type Response struct {
+  Message string `json:"message"`
+  Ok bool `json:"ok"`
+}
+
+func Handler(request Request) (Response, error) {
+  return Response {
+    Message: fmt.Sprintf("Request ID %f", request.ID),
+    Ok: true,
+  }, nil
+}
+
+func main() {
+  lambda.Start(Handler)
+}
